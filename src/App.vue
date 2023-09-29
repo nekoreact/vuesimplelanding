994ebf125031ac4899b903eae6323a8be361a4b0
Vic dev <victor.moreno@mysevensuite.com>
2023-09-28 23:48:57 -0400

    simple landing V1

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<template>
+  <header class="full-header w-full">
+    <div class="overlayd">
+      <div class="titles">
+        <img src="./assets/ziklag.png">
+        <div class="language-switcher">
+            <span>
+              <img src="./assets/en.png">  
+              English
+            </span> 
+            <span>
+              <img src="./assets/es.png">  
+              Espanol
+            </span>
+            <span>
+              <img src="./assets/pt.png">  
+              Portugues
+            </span>
+            <span>
+              <img src="./assets/em.png">  
+              Arabe
+            </span>
+        </div>
+      </div>
+      <nav class="first-nav">
+        <RouterLink to="/">Inicio</RouterLink>
+        <a href="#services">Servicios</a>
+        <RouterLink to="/about">Productos</RouterLink>
+        <RouterLink to="/about">Solicitar Cotizacion</RouterLink>
+        <a href="#us">Nosotros</a>
+        <RouterLink to="/about">Contactame</RouterLink>
+      </nav>
+      <nav class="responsive-nav">
+        <div class="">
+          <span class="toggler">
+            <img src="./assets/menu.png">
+          </span>
+        </div>
+        <ul>
+          <li>
+            <RouterLink to="/">Inicio</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/about">Servicios</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/about">Productos</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/about">Solicitar Cotizacion</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/about">Idea de Flete</RouterLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,514 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <div class="w-full">
+      <div class="videobg">
+        <video autoplay muted loop>
+            <source src="../assets/video/v1.mp4" type="video/mp4" />
+        </video>
+        <div class="overlay" style="text-align: center;">
+          <h1>ZIKLAG CORPORATION</h1>
+          <span class="xcvx">
+            <span>
+              Excelencia Ante Todo
+            </span>
+          </span>
+        </div>
+      </div>
+    </div>
+    
+    
+    <div id="us" class="white-bar" style="height: 86px;">
+      <span>Unete a la mision Ziklag</span>
+    </div>
+    <div class="k-block">
+      <div class="left" style="height: 390px; overflow: hidden;">
+        <img src="../assets/ziklag9.jpg" style="position: relative; top: -190px;">
+      </div>
+      <div class="right">
+        <h2>Nuestra Vision</h2>
+        <span>
+          <p>
+            Proveer a nuestros clientes de la mejor experiencia y calidad de nuestros servicios 
+            y productos, crear equipos de trabajo que sean valorados y capacitados de las mejores
+            herramientas para siempre estar a la vanguardia e innovacion del mercado con una expancion
+            de crecimiento firme con cimientos que vayan de generacion a generacion 
+          </p>
+        </span>
+      </div>
+    </div>
+
+    <div class="k-block">
+      <div class="right" style="padding: 1rem 2rem;">
+        <h2>Nuestra Mision</h2>
+        <span>
+          <p>
+            En Ziklag Corporation hoy dia nuestra mision no esta lejos de nuestras metas y suenos
+            pues hemos sido bendecidos con equipos de profesionales comprometidos con nuestra Vision
+            aportando calidad y precision en todas las areas de la empresa, nos sentimos realizados
+            y orgullosos de nuestros logros que hemos tenido, que tenemos y tendremos y el camino
+            es adelante, Full Avanti
+          </p>
+        </span>
+      </div>
+      <div class="left" style="height: 390px; overflow: hidden;">
+        <img src="../assets/ziklag10.jpg" style="height: 100%;">
+      </div>
+    </div>
+    <div class="w-full">
+        <div class="white-bar">
+          <span>Conoce Nuestros Alcances</span>
+        </div>
+    </div>
+
+    <div class="promo-block">
+      <div class="left">
+        
+      </div>
+      <div class="right">
+        
+      </div>
+    </div>
+
+    <section class="infopage">
+      <h1>
+        En Ziklag Corporation
+      </h1>
+      <span>
+        nos especializamos en logistica maritima aerea y terreste,
+        Atendemos empresas nacionales e internacionales, con cargas a granel y cargas especializadas.
+        Ofrecemos buques con Bodega, quimiqueros, Petroleros, Roros, Gabarras entre otros.
+        Asi mismo realizamos las operaciones logistica en tierra, con diferentes destinos, contamos con almacen de 
+        resguardo y entrega de puerta a puerta. en nuestra division de commodities,
+        puedes optar por solicitar, materia prima para produccion de alimentos, 
+        fertilizantes, quimicos, bases, y mas, de tal manera, proveemos material de agregados y material de construccion.
+        En nuestra corporacion ofrecemos calidad de ingenieria de telecomunicaciones y tecnologia, la misma desarrolla aplicaciones, 
+        y sistemas de administracion empresarial, industria hotelera, clinicas, hospitales,
+        colegios, universidades, aeropuertos, centros comerciales y tiendas, desarrollando un sistema hecho a la medida para cada necesidad,
+        de facil operatividad que pueda ayudar a administrar operaciones logistica de proveedores, gerencias, departamentos, finanzas, almacenes
+        e inventarios. todos pueden ser operados online a distancias remotas, la misma puede emitir, reportes en general, 
+        desde informacion diaria hasta informacion anual.
+        optimizando recursos indispensables para desarrollar proyectos que necesitan de varias etapas, 
+        inyeccion financiera, proyecto y la disposicion de ejercerlo. en Ziklag corporation contamos con un background de 
+        primera linea, donde podemos desarrollar tu proyecto con llave en mano
+        utilizando garantias liquidas e intrumentos que nos respaldar la operacion. 
+        Para garantizar la operacion y el desarrollo de urbanizmos, 
+        vialidades, centros empresariales, edificios residenciales, infraestructura en general, puertos, aeropuertos, 
+        aunado a industrias petroleras y energeticas.
+      </span>
+    </section>
+
+
+    <div id="services" class="white-bar" style="height: 41px; margin-top: 20px;">
+      <span>Servicios que ofrecemos</span>
+    </div>
+
+    <div class="new-block">
+      <div class="">
+        <div class="">
+          <span class="xt">LOGISTICA FLETAMENTO MARITIMOS</span>
+        </div>
+    
+        <div class="w-full flexer">
+          <div class="promo-group">
+            <div class="icongroup">
+              <img src="../assets/icon1.png">
+            </div>
+            <span class="titler">OPERADOR LOGISTICO DE CARGA</span>
+            <div class="subtitler">
+              SE ENCARGA DE LA GESTION DE BUSQUEDA Y CIERRE DEL FLETAMENTO MARITIMO A LOS CLIENTES.
+            </div>
+          </div>
+    
+          <div class="promo-group">
+            <div class="icongroup">
+              <img src="../assets/icon2.png" style="
+    position: relative;
+    top: 22px;
+">
+            </div>
+            <span class="titler">LOGISTICAS DE CONTENEDORES MARITIMOS</span>
+            <div class="subtitler">
+              OFRECEMOS CONTENEDOR DE 20, 40, 45 FT Y REEFER, ALMACENAMIENTO CARGA, 
+              DESCARGA Y AGENCIAMIENTO ADUANAL.
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="">
+        <div class="">
+          <span class="xt">LOGISTICA FLETE AEREA</span>
+        </div>
+    
+        <div class="w-full flexer" style="
+    margin-top: -36px;
+">
+          <div class="promo-group">
+            <div class="icongroup">
+              <img src="../assets/icon3.png" style="position: relative; top: 22px;">
+            </div>
+            <span class="titler">OPERADOR LOGISTICO DE CARGA</span>
+            <div class="subtitler">
+              SE ENCARGA DE LA GESTION DE BUSQUEDA Y CIERRE DEL FLETE AEREO INTERNACIONAL.
+            </div>
+          </div>
+    
+          <div class="promo-group">
+            <div class="icongroup">
+              <img src="../assets/icon4.png" style="position: relative; top: 22px;">
+            </div>
+            <span class="titler">LOGISTICAS DE CARGA AEREA</span>
+            <div class="subtitler">
+              OFRECEMOS PALLET Y CARGAS INDIVUALES DE PUERTA EN PUERTA Y CONTAMOS
+              CON ALMACEN EN DIFERENTES DESTINOS.
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div class="">
+        <div class="">
+          <span class="xt">LOGISTICA TRANSPORTE TERRESTRE</span>
+        </div>
+    
+        <div class="w-full flexer" style="
+    margin-top: -36px;
+">
+          <div class="promo-group">
+            <div class="icongroup">
+              <img src="../assets/icon5.png" style="position: relative; top: 22px;">
+            </div>
+            <span class="titler">OPERADOR LOGISTICO DE CARGA TERRESTRE</span>
+            <div class="subtitler">
+              SE ENCARGA DE GESTIONAR BUSQUEDA Y CIERRE DEL FLETE DE CARGA.
+            </div>
+          </div>
+    
+          <div class="promo-group">
+            <div class="icongroup">
+              <img src="../assets/icon6.png" style="position: relative; top: 22px;">
+            </div>
+            <span class="titler">DISTRIBUCION Y ALMACENAMIENTO DE CARGA</span>
+            <div class="subtitler">
+              OFRECEMOS ALMACEN CADENA DE FRIO Y CARGA ESPECIAL
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    
+    
+
+    <div class="promo-block mg">
+      <div class="left">
+        
+      </div>
+      <div class="right">
+        
+      </div>
+    </div>
+
+    
+
+
+
+    <div id="services" class="white-bar" style="height: 41px; margin-top: 20px;">
+      <span>Productos que ofrecemos</span>
+    </div>
+    
+    
+
+    <div class="new-block">
+      <div class="">
+        <div class="">
+          <span class="xt">MATERIAS PRIMAS ALIMENTO</span>
+        </div>
+    
+        <div class="w-full flexer">
+          <div class="promo-group">
+            <div class="icongroup">
+              <img src="../assets/sugar.png">
+            </div>
+            <span class="titler">AZUCAR</span>
+            <span class="" style="font-size: 19px;">DISPONIBILIDAD: Saco 50kg - Big Bag 1.5TON</span>
+            <div class="subtitler">
+              IC45 / IC150 / IC600 / IC1200 / BROWN SUGAR 
+            </div>
+          </div>
+    
+          <div class="promo-group">
+            <div class="icongroup">
+              <img src="../assets/coffee.png" style="
+    position: relative;
+    top: 22px;
+">
+            </div>
+            <span class="titler">CAFE</span>
+            <span class="" style="font-size: 19px;">DISPONIBILIDAD: Saco 50kg - Big Bag 1.5TON</span>
+            <div class="subtitler">
+              CAFE EN GRANO. TOSTE MEDIO, TOSTE ALTO, TOSTE GOURMET 
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="">
+        <div class="w-full flexer" style="
+    margin-top: -36px;
+">
+          <div class="promo-group">
+            <div class="icongroup">
+              <img src="../assets/corn.png" style="position: relative; top: 12px;">
+            </div>
+            <span class="titler">Maiz</span>
+            <span class="" style="font-size: 19px;">DISPONIBILIDAD: Saco 50kg - Big Bag 1.5TON</span>
+            <div class="subtitler">
+              AMARILLO Y BLANCO, EN GRANOS
+            </div>
+          </div>
+    
+          <div class="promo-group">
+            <div class="icongroup">
+              <img src="../assets/salt.png" style="position: relative; top: 22px;">
+            </div>
+            <span class="titler">SAL</span>
+            <span class="" style="font-size: 19px;">DISPONIBILIDAD: Saco 50kg - Big Bag 1.5TON</span>
+            <div class="subtitler">
+              A GRANEL
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div class="">
+        <div class="w-full flexer" style="
+    margin-top: -36px;
+">
+          <div class="promo-group">
+            <div class="icongroup">
+              <img src="../assets/soja.png" style="position: relative; top: 22px;">
+            </div>
+            <span class="titler">SOJA</span>
+            <span class="" style="font-size: 19px;">DISPONIBILIDAD: Saco 50kg - Big Bag 1.5TON</span>
+            <div class="subtitler">
+              EN GRANOS Y MOLIDA 
+            </div>
+          </div>
+    
+          <div class="promo-group">
+            <div class="icongroup">
+              <img src="../assets/trigo.png" style="position: relative; top: 15px;">
+            </div>
+            <span class="titler">TRIGO</span>
+            <span class="" style="font-size: 19px;">DISPONIBILIDAD: Saco 50kg - Big Bag 1.5TON</span>
+            <div class="subtitler">
+              EN GRANOS Y MOLIDA 
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="promo-block mg2">
+      <div class="left">
+        
+      </div>
+      <div class="right">
+        
+      </div>
+    </div>
+
+    <div class="new-block">
+      <div class="">
+        <div class="">
+          <span class="xt">MATERIAS PRIMAS QUIMICA Y FERROSA</span>
+        </div>
+    
+        <div class="w-full flexer">
+          <div class="promo-group">
+            <div class="icongroup">
+              <img src="../assets/fertilizante.png">
+            </div>
+            <span class="titler">UREA</span>
+            <span class="" style="font-size: 19px;">DISPONIBILIDAD: A GRANEL</span>
+          </div>
+    
+          <div class="promo-group">
+            <div class="icongroup">
+              <img src="../assets/aluminio.png" style="
+    position: relative;
+    top: 10px;
+">
+            </div>
+            <span class="titler">BOBINA DE METAL</span>
+            <span class="" style="font-size: 19px;">DISPONIBILIDAD: A GRANEL</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="new-block">
+      <div class="">
+        <div class="w-full flexer">
+          <div class="promo-group">
+            <div class="icongroup">
+              <img src="../assets/acero.png">
+            </div>
+            <span class="titler">PALANQUILLA DE HIERRO</span>
+            <span class="" style="font-size: 19px;">DISPONIBILIDAD: A Granel</span>
+          </div>
+    
+          <div class="promo-group">
+            <div class="icongroup">
+              <img src="../assets/briqueta.png" style="
+    position: relative;
+    top: 10px;
+">
+            </div>
+            <span class="titler">BRIQUETA DE METAL</span>
+            <span class="" style="font-size: 19px;">DISPONIBILIDAD: A GRANEL</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="new-block">
+      <div class="">
+        <div class="w-full flexer">
+          <div class="promo-group">
+            <div class="icongroup">
+              <img src="../assets/arena.png">
+            </div>
+            <span class="titler">FINO DE HIERRO</span>
+            <span class="" style="font-size: 19px;">DISPONIBILIDAD: A Granel</span>
+          </div>
+    
+          <div class="promo-group">
+            <div class="icongroup">
+              <img src="../assets/arena1.png" style="
+    position: relative;
+    top: 10px;
+">
+            </div>
+            <span class="titler">ALUMINA</span>
+            <span class="" style="font-size: 19px;">DISPONIBILIDAD: A GRANEL</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="new-block">
+      <div class="">
+        <div class="w-full flexer">
+          <div class="promo-group">
+            <div class="icongroup">
+              <img src="../assets/arena.png">
+            </div>
+            <span class="titler">COQUE</span>
+            <span class="" style="font-size: 19px;">DISPONIBILIDAD: A Granel</span>
+          </div>
+    
+          <div class="promo-group">
+            <div class="icongroup">
+              <img src="../assets/metanol.png" style="
+    position: relative;
+    top: 10px;
+">
+            </div>
+            <span class="titler">METANOL</span>
+            <span class="" style="font-size: 19px;">DISPONIBILIDAD: A GRANEL</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="promo-block mg5">
+      <div class="left">
+        
+      </div>
+      <div class="right">
+        
+      </div>
+    </div>
+
+    <div class="new-block">
+      <div class="">
+        <div class="w-full flexer">
+          <div class="promo-group">
+            <div class="icongroup">
+              <img src="../assets/tronco.png">
+            </div>
+            <span class="titler">MADERA</span>
+            <span class="" style="font-size: 19px;">DISPONIBILIDAD: A Granel</span>
+            <div class="subtitler">
+              MADERA TECA, CEDRO, CAOBA, ROBLE, CEREZO, OLIVO, FRESNO, OLMO
+            </div>
+          </div>
+          <div class="promo-group">
+            <div class="icongroup">
+              <img src="../assets/pino.png">
+            </div>
+            <span class="titler">PINO</span>
+            <span class="" style="font-size: 19px;">DISPONIBILIDAD: A Granel</span>
+            <div class="subtitler">
+              OFRECEMOS PINO CARIBENO
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="promo-block mg7">
+      <div class="left">
+        
+      </div>
+      <div class="right">
+        
+      </div>
+    </div>
+    <div class="w-full">
+      <div style="display: flex; align-items: center; justify-content: center; padding: 5rem;">
+        <div class="" style="
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    color: #000;
+    font-size: 43px;
+">
+          <img style="width: 80px;" src="../assets/mail.png">
+          <span>contact@ziklagcorporation.com</span>
+          <span>ventas@ziklagcorporation.com</span>
+        </div>
+        <div class="" style="
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            color: #000;
+            font-size: 43px;
+        ">
+          <img style="width: 80px;" src="../assets/wa.png">
+          <span>+584125145291</span>
+        </div>
+      </div>
+    </div>
+    
+  
+    <div class="w-full last-check"></div>
+
+    <footer style="width: 100%;">
+      <div class="" style="width: 100%; text-align: center;">
+        <span style="text-align: center;color: #000; width: 100%;">Copyright © 2023 Ziklag Corporation</span>
+      </div>
+    </footer>
+
+    
+
+    <!-- <TheWelcome /> -->
+  </main>
+</template>
